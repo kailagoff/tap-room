cfaa5b87bff19b7ae26cd735ebb26db4884c3b54
Kaila Goff and Maggie Summers <kaila@>
2018-03-28 10:24:19 -0700

    add minor styling and bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,15 @@ import { Keg } from './models/keg.model'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Tap Room';
 
   masterKegsList: Keg[] = [
-    new Keg("Fuzztail", "Sunriver", "5", "5.0"),
-    new Keg("Mosaic Pale", "Backwoods", "6", "4.7"),
-    new Keg("Tarty to the Party", "Wild Ride", "6", "6.3"),
-    new Keg("Full of Fire", "Wayfinder", "9", "7.0")
+    new Keg("Fuzztail", "Sunriver", 5, 5.0),
+    new Keg("Mosaic Pale", "Backwoods", 6, 4.7),
+    new Keg("Tarty to the Party", "Wild Ride", 6, 6.3),
+    new Keg("Full of Fire", "Wayfinder", 9, 7.0)
   ];
 
   addKeg(newKeg: Keg) {
