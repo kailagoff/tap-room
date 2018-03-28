bc9ed5979a54bbf38d3355a59324ec8d3414ed4c
Kaila Goff and Maggie Summers <maggie@>
2018-03-28 09:43:28 -0700

    show list of Tap Room Kegs available

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Ouput } from '@angular/core';
+import { Keg } from './models/keg.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tap Room';
+
+  masterKegsList: Keg[] = [
+    new Keg("Fuzztail", "Sunriver", 5, 5.0),
+    new Keg("Mosaic Pale", "Backwoods", 6, 4.7),
+    new Keg("Tarty to the Party", "Wild Ride", 6, 6.3),
+    new Keg("Full of Fire", "Wayfinder", 9, 7.0)
+  ];
+
+  
 }
